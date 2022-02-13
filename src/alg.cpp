3b74a18c29e248f578a1a2c442d0d4e46b6a6542
GolubDenis <99288665+GolubDenis@users.noreply.github.com>
2022-02-13 23:32:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,10 +2,10 @@
 
 int gcd(int a, int b) {
     while ((a != 0) && (b != 0)) {
-        if (a > b) { 
-            a = a % b; 
-        } else { 
-            b = b % a; 
+        if (a > b) {
+            a = a % b;
+        } else {
+            b = b % a;
         }
     }
     return(a + b);
